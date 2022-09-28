593e56ed312d4f0d637cfe4c0a8d77a9fef51abb
Hang Chau Nhat Nguyen <hangcnnguyen@kms.com.vn>
2022-09-28 09:36:41 +0700

    Commit code

---
+++ src/Lab_04/lab_4_all.java
@@ -0,0 +1,80 @@
+package Lab_04;
+
+import java.security.SecureRandom;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+import static java.util.Arrays.asList;
+
+
+public class lab_4_all {
+    public static void printMenu() {
+        System.out.println("---------MENU--------");
+        System.out.println("1. Add number into ArrayList");
+        System.out.println("2. Print number");
+        System.out.println("3. Get maximum number");
+        System.out.println("4. Get minimum number");
+    }
+
+    public static int getUserOption(){
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Please select option: ");
+        return scanner.nextInt();
+    }
+
+    public static void main(String[] args) {
+
+        int options = getUserOption();
+        printMenu();
+        List<Integer> arrNumber = new ArrayList<>();
+        arrNumber.add(4);
+        arrNumber.add(7);
+        arrNumber.add(14);
+        arrNumber.add(22);
+        System.out.println("A Number Arraylist: "+ arrNumber);
+
+        switch (options) {
+            case 1:
+                arrNumber.add(3, 9);
+                System.out.println("1. Phan tu them moi: " + arrNumber.get(1));
+                break;
+
+            case 2:
+                System.out.println("2. Print number");
+                int index;
+                for (index = 0; index < arrNumber.size(); index++) {
+
+                    arrNumber.get(index);
+                    System.out.println(arrNumber.get(index));
+                }
+                break;
+
+            case 3:
+                System.out.println("3. Get maximum number");
+                int maxArray;
+                maxArray = arrNumber.get(0);
+                for (int i = 0; i < arrNumber.size(); i++) {
+                    if (arrNumber.get(i) > maxArray) {
+                        maxArray = arrNumber.get(i);
+                    }
+                }
+                System.out.println("MaxArray: " + maxArray);
+                break;
+
+            case 4:
+                System.out.println("4. Get minimun number");
+                int minArray;
+                minArray = arrNumber.get(0);
+                for (int i = 0; i < arrNumber.size(); i++) {
+                    if (arrNumber.get(i) < minArray) {
+                        minArray = arrNumber.get(i);
+                    }
+                }
+                System.out.println("MinArray: " + minArray);
+                break;
+            default:
+                System.out.println("Invalid date");
+        }
+    }
+}
